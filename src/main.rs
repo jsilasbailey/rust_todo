adc3e3b389d21d87c54c51ba60d0b83d70bec425
Jesse Bailey <jsilasbailey@thoughtbot.com>
2021-12-23 17:54:20 -0500

    Use args as iterator
    Chapter 13.3 of the Rust Book explains how to use external args in their
    iterator form.
    
    This change modifies parsing args and passing them to Config based on
    that example.

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@ use std::process;
 use rust_todo::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::from_args(&args).unwrap_or_else(|err| {
+    let config = Config::from_args(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
