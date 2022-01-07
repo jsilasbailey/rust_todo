15515f9e0762606fdbd9b9a4ab65b9c752190820
Jesse Bailey <jsilasbailey@thoughtbot.com>
2022-01-07 11:21:34 -0500

    Add list todos command
    Adds a command for listing contents of the local todo.txt file. Notifies
    the use if they attempt to list todos and a todo.txt file does not yet
    exist in the current context.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use std::io::prelude::*;
 
 pub enum Command {
     CreateTodo,
+    ListTodos,
     Unsupported,
 }
 
@@ -17,21 +18,35 @@ impl Config {
         args.next();
 
         let command = args.next().ok_or("Please specify a command!")?;
-        let target = args.next().ok_or("Please specify a target!")?;
-
         let command = match command.as_str() {
             "a" => Command::CreateTodo,
             "add" => Command::CreateTodo,
+            "l" => Command::ListTodos,
+            "list" => Command::ListTodos,
             _ => Command::Unsupported,
         };
 
-        Ok(Config { command, target })
+        match command {
+            Command::CreateTodo => {
+                let target = args.next().ok_or("Please specify a target!")?;
+                Ok(Config { command, target })
+            }
+            Command::ListTodos => Ok(Config {
+                command,
+                target: String::from("todo.txt"),
+            }),
+            Command::Unsupported => Ok(Config {
+                command,
+                target: String::from(""),
+            }),
+        }
     }
 }
 
 pub fn run(config: Config) -> Result<(), String> {
     match config.command {
         Command::CreateTodo => create_todo(&config.target).or_else(|err| Err(err.to_string())),
+        Command::ListTodos => list_todos().or_else(|err| Err(err.to_string())),
         Command::Unsupported => Err(String::from("Unsupported command!")),
     }
 }
@@ -45,4 +60,28 @@ fn create_todo(todo: &str) -> Result<(), std::io::Error> {
     writeln!(file, "{}", todo)?;
 
     Ok(())
+}
+
+fn list_todos() -> Result<(), std::io::Error> {
+    let file = OpenOptions::new().read(true).open("todo.txt");
+
+    match file {
+        Ok(file) => {
+            for (index, line) in std::io::BufReader::new(file).lines().enumerate() {
+                if let Ok(todo_line) = line {
+                    println!("{} {}", index + 1, todo_line);
+                }
+            }
+
+            Ok(())
+        }
+        Err(error) => match error.kind() {
+            std::io::ErrorKind::NotFound => {
+                println!("No todo.txt file yet!");
+
+                Ok(())
+            }
+            _ => Err(error),
+        },
+    }
 }
